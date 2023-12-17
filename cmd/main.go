56be4f74788e26d737f35859d93721af423e7324
egesoyarar <egesoyarar@gmail.com>
2023-12-18 01:07:32 +0300

    player scores are added. smt wrong after the player6.

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"math/rand"
 	"nba-simulation/pkg/config"
@@ -11,10 +12,13 @@ import (
 	"time"
 )
 
+var playerChoices = []int{0, 1, 2, 3, 4}
+
 var (
-	homeScore int
-	awayScore int
-	mu        sync.Mutex
+	homeScore                                                                                 int
+	awayScore                                                                                 int
+	player1, player2, player3, player4, player5, player6, player7, player8, player9, player10 int
+	mu                                                                                        sync.Mutex
 )
 
 func main() {
@@ -65,7 +69,7 @@ func getScoresHandler(w http.ResponseWriter, r *http.Request) {
 	defer mu.Unlock()
 
 	// Return the current scores as JSON
-	scoresCopy := [2]int{homeScore, awayScore}
+	scoresCopy := [12]int{homeScore, awayScore, player1, player2, player3, player4, player5, player6, player7, player8, player9, player10}
 	scoresJSON, err := json.Marshal(scoresCopy)
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -79,7 +83,14 @@ func getScoresHandler(w http.ResponseWriter, r *http.Request) {
 func simulateAttack(team *entities.Team) {
 	points := randomScoring()
 
-	team.Points += points
+	if points != 0 {
+		scorerOrder, assistOrder := randomPickScorerAssist()
+		team.Points += points
+		team.Players[scorerOrder].Points += points
+		team.Players[assistOrder].Assists += 1
+		fmt.Printf("%v scores %v points and assisted by %v\n", team.Players[scorerOrder].Name, points, team.Players[assistOrder].Name)
+	}
+
 }
 
 func randomScoring() int {
@@ -97,6 +108,23 @@ func updateScores(homeTeam, awayTeam *entities.Team, totalGameDuration int) {
 		simulateAttack(awayTeam)
 		homeScore = homeTeam.Points
 		awayScore = awayTeam.Points
+		player1 = homeTeam.Players[0].Points
+		player2 = homeTeam.Players[1].Points
+		player3 = homeTeam.Players[2].Points
+		player4 = homeTeam.Players[3].Points
+		player5 = homeTeam.Players[4].Points
+		player6 = awayTeam.Players[0].Points
+		player7 = awayTeam.Players[1].Points
+		player8 = awayTeam.Players[2].Points
+		player9 = awayTeam.Players[3].Points
+		player10 = awayTeam.Players[4].Points
 		mu.Unlock()
 	}
+}
+
+func randomPickScorerAssist() (int, int) {
+	scorer := rand.Intn(5)
+	restPlayerChoices := append(playerChoices[:scorer], playerChoices[scorer+1:]...)
+	assist := rand.Intn(len(restPlayerChoices))
+	return scorer, assist
 }
